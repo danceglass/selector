e20ad8ecde10e3cd45b7bdc8c1adca788ef596c7
krynen <krynen2530@gmail.com>
2017-08-12 05:45:39 +0900

    align method change and apply animation

--- js/import_data.js
+++ js/import_data.js
@@ -75,7 +75,7 @@ function init(dcconList) {
 		}
 		
 		
-		if (message.search(/\s*<[^<>]*>\s*/g) == 0) {
+		if (message.replace(/\s*<[^<>]*>\s*/g, "").length == 0) {
 			message = '<center>' + message + '</center>'; }
 		return message;
 	}
